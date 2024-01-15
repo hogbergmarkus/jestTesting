dd5f0905049639c8f0ec479389b3b095a573264d
Markus Högberg <m.o.hogberg@gmail.com>
2024-01-15 10:44:59 +0000

    Add test of the DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,19 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContetnts = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContetnts);
+    document.close();
 })
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    })
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     })
 })
